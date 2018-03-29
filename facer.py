6fdca5a6c1ba28b1226c406b6eeff9b0a8e3d0b3
Ege Ismail Kosedag - Septillioner <septillioner@protonmail.com>
2018-03-29 22:36:11 +0300

    Fixed Bugs v0.1b6

--- facer.py
+++ facer.py
@@ -3,10 +3,10 @@ import numpy as np
 import os
 import json as JsonParser
 import hashlib as hlb
-FCR_VERSION = "0.1a"
+FCR_VERSION = "0.1b"
 def md5(text):
     d_b = hlb.md5()
-    d_b.update(text)
+    d_b.update(text.encode("utf-8"))
     return d_b.hexdigest()
 class Facer:
     FCR_Algorithms = np.array([md5("Eigen_FR"),
@@ -16,36 +16,36 @@ class Facer:
         self.options = dict(options)
         self.facer_LF = False
         self.CWDType = True
-        if options.has_key("facer_dir"):
-            if os.path.exists(os.path.join(os.getcwd(),options["facer_dir"])):
+        if "fcr_dir" in options:
+            print("ok")
+            if os.path.exists(options["fcr_dir"]):
                 self.CWDType = True
             else:
-                os.makedirs(os.path.join(os.getcwd(),options["facer_dir"]))
+                os.makedirs(options["fcr_dir"])
                 self.CWDType = True
-        elif options.has_key("facer_name"):
-            self.facer_Name = options["facer_name"]
+        elif "fcr_Name" in options:
+            self.facer_Name = options["fcr_Name"]
             if os.path.exists(os.path.join(os.getcwd(),self.facer_Name)):
                 self.CWDType = False
             else:
-                os.makedirs(os.path.join(os.getcwd(),self.facer_name))
+                os.makedirs(os.path.join(os.getcwd(),self.facer_Name))
                 self.CWDType = False
         if self.CWDType:
-            pass
+            cwd = options["fcr_dir"]
         else:
             cwd = os.path.join(os.getcwd(),self.facer_Name)            
         if os.path.isfile(os.path.join(cwd,"foptions.json")):
             mjd = JsonParser.loads(os.path.join(cwd,"foptions.json"))
             Filter = (
-                        mjd.has_key("fcr_name") and
-                        mjd.has_key("fcr_version") and
-                        mjd.has_key("fcr_file") and
-                        mjd.has_key("fcr_algorithm")
+                        "fcr_name" in mjd and
+                        "fcr_version" in mjd and
+                        "fcr_file" in mjd and
+                        "fcr_algorithm" in mjd
                     )
             if Filter:
                 mfd = os.path.join(cwd,mjd["fcr_file"])
                 Filter2 = (
                             mjd["fcr_version"] == FCR_VERSION and
-                            os.path.isfile(mfd) and
                             (np.array(mjd["fcr_algorithm"]) == FCR_Algorithms).any()
                         )
                 if Filter2:
@@ -56,7 +56,21 @@ class Facer:
                     if self.facer_Recognizer is not None:
                         self.facer_LF = True
                     else:
-                        print("Facer Version Error or Facer File Error")
+                        return None
+                else:
+                    return None
+            else:
+                return None
+        else:
+            self.facer_Recognizer = self.GetAlgorithm(self.FCR_Algorithms[2])
+            self.facer_Recognizer.write("Untitled.xml")
+            with open(os.path.join(cwd,"foptions.json"),"w") as fp:
+                fp.write(JsonParser.dumps({
+                    "fcr_name":"Untitled",
+                    "fcr_version":FCR_VERSION,
+                    "fcr_file":"Untitled.xml",
+                    "fcr_algorithm":self.FCR_Algorithms[2]
+                }))
     def GetAlgorithm(self,algoid):
         if(algoid == self.FCR_Algorithms[0]):
             return cv2.face.EigenFaceRecognizer_create()
@@ -66,3 +80,7 @@ class Facer:
             return cv2.face.LBPHFaceRecognizer_create()
         else:
             return None
+def main():
+    FTL = Facer({"fcr_Name":"sepy"})
+if __name__ == '__main__':
+    main()
